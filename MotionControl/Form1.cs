4e6e1e8214a041c1d9080e59842996f800e86eac
Forever <1727365097@qq.com>
2022-11-21 20:59:07 +0800

    添加项目文件。

---
+++ MotionControl/Form1.cs
@@ -0,0 +1,28 @@
+﻿using MotionControl.MotionClass;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MotionControl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            var a= MotionBase.GetClassType(MotionBase.CardName.LeiSai);
+            a.CardErrorMessageEvent += (i, message) =>
+            {
+                Console.WriteLine(i.ToString(),message);
+            };
+            a.CardErrorMessage(1);
+            a.OpenCard(new ushort[0]);
+        }
+    }
+}
---
+++ MotionControl/MotionClass/IControlBaseInterface.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MotionControl.MotionClass
+{
+    interface IControlBaseInterface
+    {
+        /// <summary>
+        /// 数字io输入
+        /// </summary>
+        bool[] IO_Intput { get; set; }
+
+        /// <summary>
+        /// 数字io输出
+        /// </summary>
+        bool[] IO_Output { get; set; }
+
+        /// <summary>
+        /// 板卡号
+        /// </summary>
+        ushort[] Card_Number { get; set; }
+
+        /// <summary>
+        /// 轴号
+        /// </summary>
+        ushort[] AxisNo { get; set; }
+
+        /// <summary>
+        /// 打开板卡
+        /// </summary>
+        /// <param name="Card_Number">板卡号</param>
+        /// <returns></returns>
+        bool OpenCard(ushort[] Card_Number);
+       
+      
+    }
+}
