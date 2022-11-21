96286a145b59932939d0bb90cb4201bb5ff6e114
Forever <1727365097@qq.com>
2022-11-21 20:59:07 +0800

    新增加接口、板卡基类，以及增加雷赛板卡方法回调错误解

---
+++ MotionControl/MotionClass/LeiSai.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotionControl.MotionClass
+{
+    internal class LeiSai : MotionBase
+    {
+        public override bool[] IO_Intput { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override bool[] IO_Output { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override ushort[] Card_Number { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override ushort[] AxisNo { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override bool OpenCard(ushort[] Card_Number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MotionControl/MotionClass/MotionBase.cs
@@ -0,0 +1,75 @@
+﻿using SQLiteHelper;
+using System;
+
+namespace MotionControl.MotionClass
+{
+    public abstract class MotionBase : IControlBaseInterface
+    {
+        public abstract bool[] IO_Intput { get; set; }
+        public abstract bool[] IO_Output { get; set; }
+        public abstract ushort[] Card_Number { get; set; }
+        public abstract ushort[] AxisNo { get; set; }
+
+        /// <summary>
+        /// 运动控制板卡方法异常事件
+        /// </summary>
+        public event Action<object, string> CardErrorMessageEvent;
+
+        /// <summary>
+        /// 板卡品牌名称
+        /// </summary>
+        public enum CardName
+        {
+            /// <summary>
+            /// 雷赛板卡
+            /// </summary>
+            LeiSai,
+            /// <summary>
+            /// 高川板卡
+            /// </summary>
+            GaoChuān,
+        }
+
+        public static string CardBrand { get; set; }
+
+        /// <summary>
+        /// 获取板卡对象
+        /// </summary>
+        /// <param name="modelname">板卡厂商名称</param>
+        /// <returns>返回板卡对象</returns>
+        /// <exception cref="Exception"> MotionBase类：GetClass方法中出现异常</exception>
+        public static MotionBase GetClassType(CardName modelname)
+        {
+            try
+            {
+                CardBrand = modelname.ToString();
+                var Assemblyname = System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Namespace;
+                Type type = Type.GetType(Assemblyname + "." + modelname.ToString());
+                dynamic obj = type.Assembly.CreateInstance(type.ToString());
+                MotionBase classBase = obj as MotionBase;
+                return classBase;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(" MotionBase类：GetClass方法中出现异常", ex);
+            }
+        }
+
+        public bool CardErrorMessage<T>(T type, int returnPattern = 1) where T : struct
+        {
+            if (Convert.ToInt64(type) != 0)
+            {
+                var data = SQLHelper.Readdata(CardBrand, Convert.ToUInt16(type));
+                CardErrorMessageEvent(type, data);
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+
+        public abstract bool OpenCard(ushort[] Card_Number);
+    }
+}
